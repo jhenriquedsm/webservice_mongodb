53088c5a072633da456f3549f37412947f6f1207
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-03 16:28:36 -0300

    initial commit

---
+++ src/main/java/com/jhenriquedsm/werbservice_mongodb/WerbserviceMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.jhenriquedsm.werbservice_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerbserviceMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerbserviceMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhenriquedsm/werbservice_mongodb/WerbserviceMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhenriquedsm.werbservice_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WerbserviceMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
