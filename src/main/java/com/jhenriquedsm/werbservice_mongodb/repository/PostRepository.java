35364ae5200642c4e0c8c91b816443f1c34b467b
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-05 15:34:13 -0300

    simple query with query methods

--- src/main/java/com/jhenriquedsm/werbservice_mongodb/repository/PostRepository.java
+++ src/main/java/com/jhenriquedsm/werbservice_mongodb/repository/PostRepository.java
@@ -3,6 +3,9 @@ package com.jhenriquedsm.werbservice_mongodb.repository;
 import com.jhenriquedsm.werbservice_mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
