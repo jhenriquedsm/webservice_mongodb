864515670257f467f82f071ef9cf3aa3f658a86d
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-05 14:50:09 -0300

    getting post by id

---
+++ src/main/java/com/jhenriquedsm/werbservice_mongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.jhenriquedsm.werbservice_mongodb.services;
+
+import com.jhenriquedsm.werbservice_mongodb.domain.Post;
+import com.jhenriquedsm.werbservice_mongodb.domain.User;
+import com.jhenriquedsm.werbservice_mongodb.dto.UserDTO;
+import com.jhenriquedsm.werbservice_mongodb.repository.PostRepository;
+import com.jhenriquedsm.werbservice_mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id) {
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+    }
+}
