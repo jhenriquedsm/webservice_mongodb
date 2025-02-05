46e75c46b3f757605516c8d3d56485632832706d
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-05 15:40:17 -0300

    simple query with @Query

--- src/main/java/com/jhenriquedsm/werbservice_mongodb/repository/PostRepository.java
+++ src/main/java/com/jhenriquedsm/werbservice_mongodb/repository/PostRepository.java
@@ -2,10 +2,14 @@ package com.jhenriquedsm.werbservice_mongodb.repository;
 
 import com.jhenriquedsm.werbservice_mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
